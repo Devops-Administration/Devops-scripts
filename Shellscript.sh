3d32ce987c7a886fa78b12dcbe4b8d95d816e12c
Korukonda <srikanth.korukonda@hp.com>
2020-02-15 21:14:13 +0530

    new deployment

---
+++ Shellscript.sh
@@ -0,0 +1,5 @@
+hi This is reagrding the git introduction and 
+How to deploy files in remote repository....
+
+
+:
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+this is another shell script which needs to be deployed to remote repository
+
+thank you
